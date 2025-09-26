a92f837171b164d231d15ccd15c0ddba5d9a71c1
Matias Alessandrello <matialess@gmail.com>
2025-09-25 21:20:59 -0300

    se agregan dos nodos al arbol

--- arbol_binario.py
+++ arbol_binario.py
@@ -1,6 +1,6 @@
 class nodo_arbol(object):
     #clase nodo arbol
-    def __init__(self,info):
+    def __init__(self, info):
         #crea un nodo con la información
         self.izquierda = None
         self.derecha = None
@@ -9,3 +9,6 @@ class nodo_arbol(object):
 class ArbolBinario:
     def __init__(self):
         self.raiz = None
+
+nodo1 = nodo_arbol(10) # Crea un nodo con valor 10
+nodo2 = nodo_arbol(5) # Crea un nodo con valor 5
