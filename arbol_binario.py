c3f6918ac92d0c3f672124db59ac037a3ff27450
Matias Alessandrello <matialess@gmail.com>
2025-09-25 21:11:34 -0300

    armando arboles binarios

---
+++ arbol_binario.py
@@ -0,0 +1,11 @@
+class nodo_arbol(object):
+    #clase nodo arbol
+    def __init__(self,info):
+        #crea un nodo con la información
+        self.izquierda = None
+        self.derecha = None
+        self.informacion = info
+
+class ArbolBinario:
+    def __init__(self):
+        slef.raiz = None
